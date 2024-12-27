20c335c84f714c4ec7954ebf2d97aec457114fc8
wisteriahuman <wisteriahuman91@gmail.com>
2024-12-27 16:26:22 +0900

    add headを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
       <>
         <html lang="ja">
+          <head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Home | wisteria.dev</title>
+          </head>
           <body>
             <header>{children}</header>
             <main></main>
