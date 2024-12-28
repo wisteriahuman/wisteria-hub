3ad65ffc9bccc3935315caef5135f5b13df814a8
wisteriahuman <wisteriahuman91@gmail.com>
2024-12-28 17:30:14 +0900

    add lodingを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Home | wisteria.dev</title>
           </head>
-          <body>
+          <body style={{ backgroundColor: "#1e1c2f" }}>
             <SearchAppBar />
             <main>{children}</main>
           </body>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
+import CircularIndeterminate from "@/components/loading/CircularIndeterminate";
+
 export default function Loading() {
     return (
-        <div>
-            <h1>Loading...</h1>
-        </div>
+        <>
+            <CircularIndeterminate />
+        </>
     );
 }
