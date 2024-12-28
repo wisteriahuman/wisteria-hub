6b21a533df44760193317eeec4e1ea44db51e747
wisteriahuman <wisteriahuman91@gmail.com>
2024-12-29 00:11:53 +0900

    add cardを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Grid from "@mui/material/Grid2";
+import CardToProjects from "@/components/cards/CardToProjects";
+import CardToProfile from "@/components/cards/CardToProfile";
+import CardToAbout from "@/components/cards/CardToAbout";
+import CardToArchive from "@/components/cards/CardToArchive";
 
 export const metadata = {
   title: "Home | wisteria.dev",
@@ -8,8 +13,20 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
-      {/* ホームページのコンテンツ */}
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+          <CardToProjects />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+          <CardToProfile />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+          <CardToAbout />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+          <CardToArchive />
+        </Grid>
+      </Grid>
     </>
   );
 }
