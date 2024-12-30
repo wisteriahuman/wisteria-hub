0a7e54fe5b3f012ab70eeb90d2e081ba9cabba5c
wisteriahuman <wisteriahuman91@gmail.com>
2024-12-30 20:54:54 +0900

    いろいろ変更

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export const metadata = {
     title: "Projects | WisteriaHub",
@@ -8,8 +10,27 @@ export const metadata = {
 export default function Projects() {
     return (
         <>
-            <h1>Projects Page</h1>
-            {/* プロジェクトページのコンテンツ */}
+            <Box sx={{ padding: 4, color: "#ffcdff"}}>
+                <Typography
+                    variant="h3"
+                    component="h1"
+                    gutterBottom
+                    sx={{
+                        fontWeight: "bold",
+                        flexGrow: 1,
+                        padding: 2,
+                        backgroundColor: "#2d1e36",
+                        border: "2px solid #cc00cc",
+                        borderRadius: 2,
+                        textAlign: "center",
+                        width: "fit-content",
+                        height: "fit-content",
+                        margin: "auto",
+                    }}
+                >
+                    私が手がけたアプリたち
+                </Typography>
+            </Box>
         </>
     );
 }
