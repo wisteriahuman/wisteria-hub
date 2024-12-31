307d5296cea8f0b8f9212533c0963a53b954bbe1
wisteriahuman <wisteriahuman91@gmail.com>
2025-01-01 02:26:33 +0900

    ファビコンの位置の調整

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+    async redirects() {
+        return [
+            {
+                source: "/:path*",
+                has: [
+                    {
+                        type: "host",
+                        value: "wisteria-io.com",
+                    },
+                ],
+                destination: "https://wisteria-io.com/:path*",
+                permanent: true,
+            },
+        ]
+    },
 };
 
 export default nextConfig;
