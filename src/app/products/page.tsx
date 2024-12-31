11c125ed5ebe93e0a780e3d00411a4765b3d0825
wisteriahuman <wisteriahuman91@gmail.com>
2025-01-01 07:28:29 +0900

    ごめんなさい

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 
 export const metadata = {
     title: "Products | WisteriaHub",
