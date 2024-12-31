5c10f5b4dc05f412769dcb61cf7f14bc76ed625b
wisteriahuman <wisteriahuman91@gmail.com>
2024-12-31 22:26:03 +0900

    GridをGrid2に置換

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Grid from "@mui/material/Grid2";
+import Grid2 from "@mui/material/Grid2";
 import CardToProjects from "@/components/cards/CardToProjects";
 import CardToProfile from "@/components/cards/CardToProfile";
 import CardToAbout from "@/components/cards/CardToAbout";
@@ -13,20 +13,20 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+      <Grid2 container spacing={2}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 6 }}>
           <CardToProjects />
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 6 }}>
           <CardToProfile />
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 6 }}>
           <CardToAbout />
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6, md: 6 }}>
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 6 }}>
           <CardToArchive />
-        </Grid>
-      </Grid>
+        </Grid2>
+      </Grid2>
     </>
   );
 }
--- src/components/hobbyInterests/hobbyInterests.tsx
+++ src/components/hobbyInterests/hobbyInterests.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Typography } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import Grid2 from "@mui/material/Grid2";
 import ListBox from "@/components/listBox/ListBox";
 
 
@@ -8,29 +8,29 @@ const HobbyInterests: React.FC = () => {
     return (
         <>
             <Typography variant="h4">趣味・関心など</Typography>
-            <Grid container spacing={2}>
-                <Grid>
+            <Grid2 container spacing={2}>
+                <Grid2>
                     <ListBox
                         title="趣味"
                         items={["プログラミング", "読書", "シャワー"]}
                         dense={false}
                     />
-                </Grid>
-                <Grid>
+                </Grid2>
+                <Grid2>
                     <ListBox
                         title="好きなもの"
                         items={["猫", "水", "クラシック", "洋楽", "アニメ"]}
                         dense={false}
                     />
-                </Grid>
-                <Grid>
+                </Grid2>
+                <Grid2>
                     <ListBox
                         title="好きな季節"
                         items={["春", "夏", "秋", "冬"]}
                         dense={false}
                     />
-                </Grid>
-            </Grid>
+                </Grid2>
+            </Grid2>
         </>
     );
 }
